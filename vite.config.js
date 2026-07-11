56192a2e99463e83fdd9e17499345ae0eec12fa4
imattau <imatt.au@protonmail.com>
2026-07-11 20:17:45 +1000

    Fix file:// demo: build as IIFE, remove type=module from dist
    - Set rollup output format to iife for non-module script output
    - Add Vite plugin to strip type=module and crossorigin from build HTML
    - Set base: './' for relative paths in dist
    - Rebuild and update wp-disgus assets

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,32 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',
   build: {
     rollupOptions: {
       output: {
+        format: 'iife',
+        name: 'Disgus',
         entryFileNames: '[name].js',
         assetFileNames: '[name][extname]'
       }
     },
-    cssCodeSplit: false
+    cssCodeSplit: false,
+    modulePreload: false,
+    target: 'es2015'
   },
   path: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'no-module-html',
+      apply: 'build',
+      transformIndexHtml: {
+        order: 'post',
+        handler(html) {
+          return html.replace(/\btype="module"\s*/g, '').replace(/\bcrossorigin\s*/g, '');
+        }
+      }
+    }
+  ],
 });
